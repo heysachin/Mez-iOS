85f4c37a39f711de634521314ba5b24f4ce27b1b
Sachin Dev <hellosachindev@gmail.com>
2019-03-18 14:23:01 +0530

    Initial Commit

---
+++ Mez iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mez iOS
+//
+//  Created by Sachin Dev on 18/03/19.
+//  Copyright © 2019 Sachin Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
